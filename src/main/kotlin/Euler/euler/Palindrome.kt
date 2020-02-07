316e640ca1195603da2c977a4d058afe796d182f
Stephen Roberts <stephen.roberts@skybettingandgaming.com>
2020-02-07 15:12:26 +0000

    Dirty answer to 4 - palindrome

--- src/main/kotlin/Euler/euler/Palindrome.kt
+++ src/main/kotlin/Euler/euler/Palindrome.kt
@@ -1,4 +1,47 @@
 package Euler.euler
 
 class Palindrome {
+    fun findLargestPalindrome(numOfDigits: Int): Int {
+
+        val largestNDigitNumber = Math.pow(10.toDouble(), (numOfDigits).toDouble()) - 1
+        val largestNumber = Math.pow(largestNDigitNumber, 2.toDouble()).toInt();
+
+        var answer: Int = 0
+
+        var array = largestNumber.toString().map { it.toString().toInt() }
+        val length = array.size
+
+        val firstHalfArray = array.subList(0, length / 2)
+        var firstHalfNumber = firstHalfArray.joinToString(separator = "") { it -> it.toString() }.toInt()
+        println(firstHalfNumber)
+
+        for (i in firstHalfNumber downTo 1) {
+
+            val iArray = i.toString().map { it.toString().toInt() }
+            var separator = emptyList<Int>()
+            var secondHalfArray = iArray.reversed()
+
+            val palindromeArray = iArray + separator + secondHalfArray
+            val palindromeNumber = palindromeArray.joinToString(separator = "") { it -> it.toString() }.toInt()
+
+            if (checkIfDivisable(palindromeNumber, largestNDigitNumber.toInt())) {
+                answer = palindromeNumber
+                break
+            }
+        }
+        return answer
+    }
+
+    fun checkIfDivisable(palindromeNumber: Int, largestDigit: Int): Boolean {
+        var x = largestDigit
+        println("Here's the palindrome: $palindromeNumber")
+        while (palindromeNumber / x < largestDigit) {
+            if (palindromeNumber % x == 0) {
+                return true
+            } else {
+                x--
+            }
+        }
+        return false
+    }
 }
--- src/test/kotlin/Euler/euler/PalindromeTest.kt
+++ src/test/kotlin/Euler/euler/PalindromeTest.kt
@@ -1,12 +1,22 @@
 package Euler.euler
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class PalindromeTest{
-    private val palindrom = Palindrome();
+    private val palindrome = Palindrome();
 
     @Test
-    fun `largest Palindrom of 2-digit numbers is 9009`(){
+    fun `largest Palindrome of 2-digit numbers is 9009`(){
+        val answer = palindrome.findLargestPalindrome(2)
 
+        assertEquals(9009, answer)
+    }
+
+    @Test
+    fun `largest Palindrome of 2-digit numbers is 906609`(){
+        val answer = palindrome.findLargestPalindrome(3)
+
+        assertEquals(906609, answer)
     }
 }
